e6e1f2ab7d096310b93ade1121ac9a3e99066c88
romene <rogerio.eire@gmail.com>
2019-02-18 09:55:48 +1300

    form Contact

--- src/components/FaleConosco.js
+++ src/components/FaleConosco.js
@@ -6,25 +6,11 @@ function FaleConosco() {
     <Container  fluid id="FaleConosco-section">
       <div className="FaleConosco-content">
           <h2 className="FaleConosco-title">Fale Conosco</h2>
-              <form name="contact" method="post">
-                  <p>
-                      <label>Your Name: <input type="text" name="name" /></label>
-                  </p>
-                  <p>
-                      <label>Your Email: <input type="email" name="email" /></label>
-                  </p>
-                  <p>
-                      <label>Your Role: <select name="role[]" multiple>
-                          <option value="leader">Leader</option>
-                          <option value="follower">Follower</option>
-                      </select></label>
-                  </p>
-                  <p>
-                      <label>Message: <textarea name="message"></textarea></label>
-                  </p>
-                  <p>
-                      <button type="submit">Send</button>
-                  </p>
+             
+              <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+                  <input type="text" name="name" />
+                  <input type="email" name="email" />
+                  <textarea name="message"></textarea>
               </form>
     </div></Container>            
   )

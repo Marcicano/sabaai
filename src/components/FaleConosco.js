b74bf59673fe02b5ce9c10ffeaa55365c97a73d9
romene <rogerio.eire@gmail.com>
2019-02-18 10:18:33 +1300

    form Contact

--- src/components/FaleConosco.js
+++ src/components/FaleConosco.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ContactForm from './ContactForm'
 import {Container,Col, Button, Form, FormGroup, Input, Row } from 'reactstrap';
 
 function FaleConosco() {
@@ -7,11 +8,8 @@ function FaleConosco() {
       <div className="FaleConosco-content">
           <h2 className="FaleConosco-title">Fale Conosco</h2>
              
-              <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-                  <input type="text" name="name" />
-                  <input type="email" name="email" />
-                  <textarea name="message"></textarea>
-              </form>
+              <ContactForm />
+              
     </div></Container>            
   )
 }
